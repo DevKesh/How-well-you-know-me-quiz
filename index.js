9b65adffae359caa7215aecae545ab1763e4b6db
Keshav Naganathan <keshavnaganathan@gmail.com>
2020-12-10 18:00:04 +0000

    initial commit

--- index.js
+++ index.js
@@ -1,113 +1,176 @@
-// var readlineSync = require("readline-sync");
+var readlineSync = require("readline-sync");
 
-// const chalk = require("chalk");
-
-// var score = 0;
-
-// console.log(chalk.bold.white.bgRedBright("THIS IS A QUIZ TO SEE HOW WELL YOU KNOW ME!")
-// );
-// console.log(chalk.cyan.bold("**********-------*********"));
-// console.log(chalk.cyan.bold("**********-------*********"));
-// console.log(chalk.cyan.bold("**********-------*********"));
-// console.log(chalk.cyan.bold("**********-------*********"));
-// console.log(chalk.cyan.bold("**********-------*********"));
-
-// var userName = readlineSync.question(chalk.bold(" What's your name?\n\n"));
-
-// console.log(chalk.red(`Welcome ${userName}!
-// How well do you know Keshav?`));
-
-// console.log("______________________________")
+const chalk = require("chalk");
 
 
-// //keep data of high score
 
-// var playerScore = [
+console.log(chalk.bold.white.bgBlue("THIS IS A QUIZ TO SEE HOW WELL YOU KNOW ME!"));
+console.log(chalk.cyan.bold("**********-------*********"));
+console.log(chalk.cyan.bold("**********-------*********"));
+console.log(chalk.cyan.bold("**********-------*********"));
+console.log(chalk.cyan.bold("**********-------*********"));
+console.log(chalk.cyan.bold("**********-------*********"));
 
-// {
-//   name: "Jill",
-//   score : 5,
-// },
+var userName = readlineSync.question(chalk.bold.black.bgWhite("Hi, please enter your name and the game will begin!\n\n"));
 
-// {
-//   name: "jack",
-//   score : 6,
-// }
+console.log(chalk.bold.green("THE GAME BEGINS NOW!"))
 
-// ];
+console.log(chalk.red(`Welcome ${userName}!
+Lets see how well you know me 😉`));
 
-// // if PlayerScore.score > PlayerScore
-
-
-// //Play function
-
-// function play(question,answer){
+console.log(chalk.black.bold("___________________________________________________"));
+console.log(chalk.black.red("___________________________________________________"));
+console.log(chalk.black.red("___________________________________________________"));
+console.log(chalk.black.bold("___________________________________________________"));
 
 
-// var userAnswer = readlineSync.question
 
-// var userAnswer = readlineSync.question(question);
 
-// if(userAnswer.toUpperCase() === answer.toUpperCase()){
 
-//   console.log(" You are right! ");
-//   score = score + 1;
 
-//   console.log(`Your score is ${score}`);
+//keep data of high score
+
+var playerScore = [
+
+{
+  name: "Jill",
+  score : 6,
+},
+
+{
+  name: "jack",
+  score : 3,
+},
+{
+
+  name: "James",
+  score: 4,
+}
+
+
+];
+
+
+//Play function
+
+function play(question,answer){
+
+
+var userAnswer = readlineSync.question
+
+var userAnswer = readlineSync.question(question);
+
+var score = 0;
+
+if(userAnswer.toUpperCase() === answer.toUpperCase()){
+
+  console.log(" You are right! ");
+  score = score + 1;
+
+  console.log(`You have scored ${score} points `);
  
-//    console.log(chalk.red("--------------"));
-//     console.log(chalk.red("--------------"));
-//      console.log(chalk.red("--------------"));
-//       console.log(chalk.red("--------------"));
-// }
-// else{
+   console.log(chalk.red("--------------"));
+    console.log(chalk.red("--------------"));
+     console.log(chalk.red("--------------"));
+      console.log(chalk.red("--------------"));
+}
+else{
 
-//   console.log(chalk.red("Wrong!"));
-//   console.log("Dont lose hope, continue answering")
-//   console.log("___________________")
-// }
+  console.log(chalk.red("Wrong!"));
+  console.log("Dont lose hope, continue answering...")
+  console.log("___________________");
+  console.log("___________________");
+}
 
-// }
 
-// // array of objects
-// var questions = [{
+}
 
-//   question : "Where do I live ? ",
-//   answer : "Bangalore"
-// },
-// {
 
-//   question : "What do I do ?",
-//   answer : "Engineering"
-// },
-// {
-//     question : "What is my fav hobby ?",
-//   answer : "Public Speaking"
 
-// },
-// {
-//     question : "What is my favourite chat ?",
-//   answer : "Sev Puri"
 
-// }
-// ];
+// array of objects
+var questions = [{
 
-// //loop
+  question : "What is my full name ?",
+  answer : "Keshav TN"
+},
+  
+  
+{
 
-// for(var i=0; i<questions.length;i++){
+  question : "Where do I live ? ",
+  answer : "Bangalore"
+},
+{
 
-//   var currentQuestion = questions[i];
-//   play(currentQuestion.question,currentQuestion.answer);
-// }
+  question : "What do I do ?",
+  answer : "Engineering"
+},
+{
+    question : "What is my fav hobby ?",
+  answer : "Public Speaking"
 
-// console.log(`Thanks for playing ${userName}!, Your final score is ${score}`);
+},
+{
+    question : "What is my favourite chat ?",
+  answer : "Sev Puri"
 
-// console.log("You can check the leaderboard here!/n")
+},
+{
+   question : "What is my favourite tv show?",
+  answer : "Friends"
 
-// for(i=0;i<playerScore.length;i++){
+},
+{
+  question : "Who is my favourite hero?",
+  answer : "Ranveer Singh"
 
-//     var highscore = playerScore[i];
-//   console.log(chalk.green(`Topping the leaderboard is ${highscore.name}  `));
-//   console.log(chalk.red("******************"));
-//   console.log(chalk.green(`Their scores are ${highscore.score} `));
-// }
+}
+];
+
+//loop
+for(i=0;i<questions.length;i++)
+{
+  var q = questions[i];
+	play(q.question,q.answer);
+}
+
+ 
+
+
+console.log(chalk.red.bold("HERE IS THE LEADERBOARD"))
+console.log("*----*-----*-----*")
+
+function showLeaderBoard(){
+
+	for(i=0;i<playerScore.length;i++)
+  
+  {
+    player = playerScore[i];
+		console.log(chalk.cyan(player.name + ' : ' + chalk.red(player.score)));
+	}
+}
+
+function highScorer(){
+
+    if(playerScore[0].score >playerScore[1].score){
+
+      console.log(chalk.bold(" The Highest scorer is Jill")); 
+    }
+      else if(playerScore[1].score>playerScore[2].score)
+      {
+          console.log(chalk.bold("The Highest scorer is Jack"));
+      }
+      else if(playerScore[3].score>playerScore[0].score)
+      {
+        console.log(chalk.bold("The Highest scorer is James"));
+      }
+        
+      }
+  
+
+
+showLeaderBoard(playerScore);
+console.log("\n")
+highScorer(playerScore);//function call
+
